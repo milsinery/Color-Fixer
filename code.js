03f5a4f845cfb4a2c7017d43b6ef410b8ea37e89
Vladimir Bykov <milsinery@icloud.com>
2021-11-20 12:17:18 +0300

    fixed speed

--- code.js
+++ code.js
@@ -7,57 +7,62 @@ const rgbToHsl = ({ r, g, b }) => {
         l: Math.round(((v + v - c) / 2) * 100),
     };
 };
+const isSameOpacity = (colorOpacity, themeOpacity) => colorOpacity === themeOpacity;
+const isSimilarHue = (colorHue, themeHue) => {
+    if (colorHue === themeHue)
+        return true;
+    return ((colorHue > themeHue && colorHue - themeHue <= 5) ||
+        (colorHue < themeHue && themeHue - colorHue <= 5));
+};
+const isSimilarSaturation = (colorSaturation, themeSaturation) => {
+    if (colorSaturation === themeSaturation)
+        return true;
+    return ((colorSaturation > themeSaturation &&
+        colorSaturation - themeSaturation <= 5) ||
+        (colorSaturation < themeSaturation &&
+            themeSaturation - colorSaturation <= 5));
+};
+const isSimilarLight = (colorLight, themeLight) => {
+    if (colorLight === themeLight)
+        return true;
+    return ((colorLight > themeLight && colorLight - themeLight <= 5) ||
+        (colorLight < themeLight && themeLight - colorLight <= 5));
+};
 const isSimilarColor = (color, themeColor, colorOpacity, themeOpacity) => {
     const { h: colorHue, s: colorSaturation, l: colorLight } = rgbToHsl(color);
-    const { h: themeHue, s: themeSaturation, l: themeLight } = rgbToHsl(themeColor);
-    const isSameOpacity = (colorOpacity, themeOpacity) => colorOpacity === themeOpacity;
-    const isSimilarHue = (colorHue, themeHue) => {
-        if (colorHue === themeHue)
-            return true;
-        return (colorHue > themeHue && colorHue - themeHue <= 5) || (colorHue < themeHue && themeHue - colorHue <= 5);
-    };
-    const isSimilarSaturation = (colorSaturation, themeSaturation) => {
-        if (colorSaturation === themeSaturation)
-            return true;
-        return ((colorSaturation > themeSaturation && colorSaturation - themeSaturation <= 5) || (colorSaturation < themeSaturation && themeSaturation - colorSaturation <= 5));
-    };
-    const isSimilarLight = (colorLight, themeLight) => {
-        if (colorLight === themeLight)
-            return true;
-        return ((colorLight > themeLight && colorLight - themeLight <= 5) || (colorLight < themeLight && themeLight - colorLight <= 5));
-    };
-    return isSameOpacity(colorOpacity, themeOpacity) && isSimilarHue(colorHue, themeHue) && isSimilarSaturation(colorSaturation, themeSaturation) && isSimilarLight(colorLight, themeLight);
+    const { h: themeHue, s: themeSaturation, l: themeLight, } = rgbToHsl(themeColor);
+    return (isSameOpacity(colorOpacity, themeOpacity) &&
+        isSimilarHue(colorHue, themeHue) &&
+        isSimilarSaturation(colorSaturation, themeSaturation) &&
+        isSimilarLight(colorLight, themeLight));
 };
 const isSimilarImages = (image, themeImage) => image === themeImage;
-const isSimilarGradient = (gradient, themeGradient) => {
-    return false;
-};
 const isSameStyles = (colors, styles) => {
     if (colors.length !== styles.length)
+        return;
+    if (colors[0].type !== styles[0].type)
         return;
     let flag = colors.length;
     for (let index = 0; index < colors.length; index++) {
         if (colors[index].visible === false)
             return;
-        if (colors[index].color !== undefined && styles[index].color !== undefined) {
-            if (colors[index].type === "SOLID" && isSimilarColor(colors[index].color, styles[index].color, colors[index].opacity, styles[index].opacity)) {
+        if (colors[index].color !== undefined &&
+            styles[index].color !== undefined) {
+            if (colors[index].type === 'SOLID' &&
+                isSimilarColor(colors[index].color, styles[index].color, colors[index].opacity, styles[index].opacity)) {
                 flag--;
             }
-            ;
-            if (colors[index].type === "GRADIENT_LINEAR" && isSimilarGradient(colors[index], styles[index])) {
-                // flag--;
-            }
-            ;
         }
-        else if (colors[index].type === "IMAGE" && isSimilarImages(colors[index].imageHash, styles[index].imageHash)) {
+        else if (colors[index].type === 'IMAGE' &&
+            isSimilarImages(colors[index].imageHash, styles[index].imageHash)) {
             flag--;
         }
-        ;
     }
     return flag === 0;
 };
 const fixColor = (node, themeStyles) => {
-    if (node.fillStyleId === themeStyles.id || node.strokeStyleId === themeStyles.id)
+    if (node.fillStyleId === themeStyles.id ||
+        node.strokeStyleId === themeStyles.id)
         return;
     if (isSameStyles(node.fills, themeStyles.paints))
         node.fillStyleId = themeStyles.id;
@@ -72,27 +77,38 @@ const fixColors = (objectsArray, stylesArray) => {
     }
 };
 const isInstanceOrParentInstance = (obj) => {
-    if (obj.type === "INSTANCE" || obj.parent.type === "INSTANCE")
+    if (obj.type === 'INSTANCE' || obj.parent.type === 'INSTANCE')
         return true;
-    if (obj.type !== "INSTANCE" && obj.parent.type !== "INSTANCE" && obj.parent.type === "PAGE") {
+    if (obj.type !== 'INSTANCE' &&
+        obj.parent.type !== 'INSTANCE' &&
+        obj.parent.type === 'PAGE') {
         return false;
     }
     else {
-        if (obj.type !== "INSTANCE" && obj.parent.type !== "INSTANCE" && obj.parent.type !== "PAGE")
+        if (obj.type !== 'INSTANCE' &&
+            obj.parent.type !== 'INSTANCE' &&
+            obj.parent.type !== 'PAGE')
             return isInstanceOrParentInstance(obj.parent);
     }
 };
 const main = () => {
     const themeStyles = figma.getLocalPaintStyles();
     if (themeStyles.length === 0) {
-        figma.notify("Layout with color styles is required");
+        figma.notify('Layout with color styles is required');
         return;
     }
-    ;
-    const allObjectsOnPage = figma.currentPage.findAll((item) => item.type === "LINE" || item.type === "ELLIPSE" || item.type === "POLYGON" || item.type === "VECTOR" || item.type === "STAR" || item.type === "RECTANGLE" || item.type === "FRAME" || item.type === "TEXT" || item.type === "COMPONENT");
-    const allFiltered = allObjectsOnPage.filter(item => item.visible && !isInstanceOrParentInstance(item));
+    const allObjectsOnPage = figma.currentPage.findAll((item) => item.type === 'LINE' ||
+        item.type === 'ELLIPSE' ||
+        item.type === 'POLYGON' ||
+        item.type === 'VECTOR' ||
+        item.type === 'STAR' ||
+        item.type === 'RECTANGLE' ||
+        item.type === 'FRAME' ||
+        item.type === 'TEXT' ||
+        item.type === 'COMPONENT');
+    const allFiltered = allObjectsOnPage.filter((item) => item.visible && !isInstanceOrParentInstance(item));
     if (allFiltered.length === 0) {
-        figma.notify("There are no items to check on this page");
+        figma.notify('There are no items to check on this page');
         return;
     }
     fixColors(allFiltered, themeStyles);
